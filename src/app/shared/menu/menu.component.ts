4f4c234f9b871cc38b9d19fc2c4437084e95b122
Saull Brandão <saull@outlook.com>
2023-01-20 13:09:33 -0300

    feat: add menu

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'labmedical-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent {
+  readonly menuCategories = [
+    {
+      title: 'geral',
+      items: [
+        { title: 'início', icon: 'list', page: '/' },
+        { title: 'sair', icon: 'logout', page: '/logout' },
+      ],
+    },
+    {
+      title: 'pacientes',
+      items: [
+        { title: 'cadastrar', icon: 'add', page: 'cadastrar-paciente' },
+        { title: 'listar prontuario', icon: 'list', page: 'pacientes' },
+      ],
+    },
+    {
+      title: 'exames',
+      items: [
+        {
+          title: 'cadastrar consulta',
+          icon: 'add',
+          page: 'cadastrar-consultas',
+        },
+        { title: 'cadastrar exame', icon: 'add', page: 'cadastrar-exames' },
+      ],
+    },
+  ];
+
+  displayButtonText: boolean = true;
+
+  toggleButtonText(toggleButton: HTMLElement) {
+    this.displayButtonText = !toggleButton.classList.contains('collapsed');
+  }
+}
